3af485ec4c123f041dfc0b9e36edc3c21a465405
annasajkh <annaskherunas06@gmail.com>
2021-08-29 20:59:04 +0700

    yay

---
+++ main.js
@@ -0,0 +1,57 @@
+function setup() {
+    createCanvas(500, 500);
+    border();
+    background(255);
+}
+
+function mouseDragged() {
+    line(mouseX, mouseY,pmouseX, pmouseY);
+}
+
+function keyPressed() {
+    if (keyCode == 32) {
+        background(255);
+    }
+}
+
+function mouseReleased() {
+    erase()
+    rect(0,0,130,40)
+    noErase()
+
+    let img = get();
+    img.resize(28, 28)
+    img.loadPixels()
+
+    let img_pixel = Array.from(img.pixels)
+    let string = ""
+
+    for(let i = 0; i < 28 * 28; i++) {
+        string += ((255 - img_pixel[i * 4]) / 255)
+        
+        if (i < 28 * 28 - 1) {
+            string += ","
+        }
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://cat-rabbit-doodle-classifier.annasvirtual.repl.co/predict_str", true);
+
+    xhr.send(JSON.stringify({"img_str": string}));
+
+    xhr.onload = () => {
+        erase()
+        rect(0,0,130,40)
+        noErase()
+        let result = JSON.parse(xhr.responseText);
+        textSize(20)
+        text("cat: " + result["cat"], 10, 20)
+        text("rabbit: " + result["rabbit"], 10, 40)
+    }
+
+
+}
+
+function draw() {
+    strokeWeight(10);
+}
